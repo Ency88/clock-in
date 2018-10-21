0a3b1afeab56871dbfeac00be087b984e8a17688
Marián Ferenc <marian.ferenc@student.tuke.sk>
2018-10-21 11:43:31 +0200

    firebase env prop

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,14 @@
 
 export const environment = {
   production: false,
+  firebase: {
+    apiKey: 'KEY',
+    authDomain: 'AUTH-DOMAIN-HERE',
+    databaseURL: 'DATABASE-URL-HERE',
+    projectId: 'PROJECT-ID',
+    storageBucket: 'STORAGE-BUCKET-HERE',
+    messagingSenderId: 'MESSAGING-SENDER-ID-HERE',
+  },
 };
 
 /*
