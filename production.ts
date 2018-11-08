bc6b6060247433696b2d999fd1c59f33530e0d0c
Soňa Kollárová <sona.kollarova.2@student.tuke.sk>
2018-11-08 20:31:22 +0100

    pull request fix

--- production.ts
+++ production.ts
@@ -9,7 +9,7 @@ export const config = {
 
 export const testUser1 = {
   email: 'admin@senacor.com',
-  password: 'admin',
+  password: 'admin123',
 };
 
 export const testUser2 = {
@@ -19,11 +19,225 @@ export const testUser2 = {
 
 export const testUser3 = {
   email: 'test@senacor.com',
-  password: 'test',
+  password: 'test123',
 };
 
 export const workingDay1 = {
-  from: 'October 30, 2018 7:30:00 AM',
-  to: 'October 30, 2018 12:30:00 AM',
-  type: 'work',
+  attendance:
+    {day: 'MO', date: new Date('October 30, 2018 7:30:00 AM'),
+    timeRecords: {
+      timeRecord1: {
+        from: new Date('October 29, 2018 7:30:00 AM'),
+        to: new Date('October 29, 2018 12:30:00 AM'),
+        type: 'work',
+      },
+      timeRecord2: {
+        from: new Date('October 29, 2018 12:30:00 AM'),
+        to: new Date('October 29, 2018 13:00:00 AM'),
+        type: 'pause',
+      },
+      timeRecord3: {
+        from: new Date('October 29, 2018 13:00:00 AM'),
+        to: new Date('October 29, 2018 15:30:00 AM'),
+        type: 'work',
+      }
+    }},
+};
+
+export const workingDay2 = {
+  attendance:
+    {day: 'TU', date: new Date('October 30, 2018 7:30:00 AM'),
+      timeRecords: {
+        timeRecord1: {
+          from: new Date('October 30, 2018 7:30:00 AM'),
+          to: new Date('October 30, 2018 12:30:00 AM'),
+          type: 'work',
+        },
+        timeRecord2: {
+          from: new Date('October 30, 2018 12:30:00 AM'),
+          to: new Date('October 30, 2018 13:00:00 AM'),
+          type: 'pause',
+        },
+        timeRecord3: {
+          from: new Date('October 30, 2018 13:00:00 AM'),
+          to: new Date('October 30, 2018 15:30:00 AM'),
+          type: 'work',
+        }
+      }},
+};
+
+export const workingDay3 = {
+  attendance:
+    {day: 'WE', date: new Date('October 31, 2018 7:30:00 AM'),
+      timeRecords: {
+        timeRecord1: {
+          from: new Date('October 31, 2018 7:30:00 AM'),
+          to: new Date('October 31, 2018 12:30:00 AM'),
+          type: 'work',
+        },
+        timeRecord2: {
+          from: new Date('October 31, 2018 12:30:00 AM'),
+          to: new Date('October 31, 2018 13:00:00 AM'),
+          type: 'pause',
+        },
+        timeRecord3: {
+          from: new Date('October 31, 2018 13:00:00 AM'),
+          to: new Date('October 31, 2018 15:30:00 AM'),
+          type: 'work',
+        }
+      }},
+};
+
+export const workingDay4 = {
+  attendance:
+    {day: 'TH', date: new Date('November 1, 2018 7:30:00 AM'),
+      timeRecords: {
+        timeRecord1: {
+          from: new Date('November 1, 2018 7:30:00 AM'),
+          to: new Date('November 1, 2018 12:30:00 AM'),
+          type: 'work',
+        },
+        timeRecord2: {
+          from: new Date('November 1, 2018 12:30:00 AM'),
+          to: new Date('November 1, 2018 13:00:00 AM'),
+          type: 'pause',
+        },
+        timeRecord3: {
+          from: new Date('November 1, 2018 13:00:00 AM'),
+          to: new Date('November 1, 2018 15:30:00 AM'),
+          type: 'work',
+        }
+      }},
+};
+
+export const workingDay5 = {
+  attendance:
+    {day: 'FR', date: new Date('November 2, 2018 7:00:00 AM'),
+      timeRecords: {
+        timeRecord1: {
+          from: new Date('November 2, 2018 7:00:00 AM'),
+          to: new Date('November 2, 2018 12:25:00 AM'),
+          type: 'work',
+        },
+        timeRecord2: {
+          from: new Date('November 2, 2018 12:25:00 AM'),
+          to: new Date('November 2, 2018 13:00:00 AM'),
+          type: 'pause',
+        },
+        timeRecord3: {
+          from: new Date('November 2, 2018 13:00:00 AM'),
+          to: new Date('November 2, 2018 15:30:00 AM'),
+          type: 'work',
+        }
+      }},
+};
+
+export const workingDay6 = {
+  attendance:
+    {day: 'MO', date: new Date('November 5, 2018 7:30:00 AM'),
+      timeRecords: {
+        timeRecord1: {
+          from: new Date('November 5, 2018 7:30:00 AM'),
+          to: new Date('November 5, 2018 12:30:00 AM'),
+          type: 'work',
+        },
+        timeRecord2: {
+          from: new Date('November 5, 2018 12:30:00 AM'),
+          to: new Date('November 5, 2018 13:00:00 AM'),
+          type: 'pause',
+        },
+        timeRecord3: {
+          from: new Date('November 5, 2018 13:00:00 AM'),
+          to: new Date('November 5, 2018 15:30:00 AM'),
+          type: 'work',
+        }
+      }},
+};
+
+export const workingDay7 = {
+  attendance:
+    {day: 'TU', date: new Date('November 6, 2018 7:30:00 AM'),
+      timeRecords: {
+        timeRecord1: {
+          from: new Date('November 6, 2018 7:30:00 AM'),
+          to: new Date('November 6, 2018 12:30:00 AM'),
+          type: 'work',
+        },
+        timeRecord2: {
+          from: new Date('November 6, 2018 12:30:00 AM'),
+          to: new Date('November 6, 2018 13:00:00 AM'),
+          type: 'pause',
+        },
+        timeRecord3: {
+          from: new Date('November 6, 2018 13:00:00 AM'),
+          to: new Date('November 6, 2018 15:30:00 AM'),
+          type: 'work',
+        }
+      }},
+};
+
+export const workingDay8 = {
+  attendance:
+    {day: 'WE', date: new Date('November 7, 2018 7:30:00 AM'),
+      timeRecords: {
+        timeRecord1: {
+          from: new Date('November 7, 2018 7:30:00 AM'),
+          to: new Date('November 7, 2018 12:30:00 AM'),
+          type: 'work',
+        },
+        timeRecord2: {
+          from: new Date('November 7, 2018 12:30:00 AM'),
+          to: new Date('November 7, 2018 13:00:00 AM'),
+          type: 'pause',
+        },
+        timeRecord3: {
+          from: new Date('November 7, 2018 13:00:00 AM'),
+          to: new Date('November 7, 2018 15:30:00 AM'),
+          type: 'work',
+        }
+      }},
+};
+
+export const workingDay9 = {
+  attendance:
+    {day: 'TH', date: new Date('November 8, 2018 7:30:00 AM'),
+      timeRecords: {
+        timeRecord1: {
+          from: new Date('November 8, 2018 7:30:00 AM'),
+          to: new Date('November 8, 2018 12:30:00 AM'),
+          type: 'work',
+        },
+        timeRecord2: {
+          from: new Date('November 8, 2018 12:30:00 AM'),
+          to: new Date('November 8, 2018 13:00:00 AM'),
+          type: 'pause',
+        },
+        timeRecord3: {
+          from: new Date('November 8, 2018 13:00:00 AM'),
+          to: new Date('November 8, 2018 15:30:00 AM'),
+          type: 'work',
+        }
+      }},
+};
+
+export const workingDay10 = {
+  attendance:
+    {day: 'FR', date: new Date('November 9, 2018 7:30:00 AM'),
+      timeRecords: {
+        timeRecord1: {
+          from: new Date('November 9, 2018 7:30:00 AM'),
+          to: new Date('November 9, 2018 12:30:00 AM'),
+          type: 'work',
+        },
+        timeRecord2: {
+          from: new Date('November 9, 2018 12:30:00 AM'),
+          to: new Date('November 9, 2018 13:10:00 AM'),
+          type: 'pause',
+        },
+        timeRecord3: {
+          from: new Date('November 9, 2018 13:10:00 AM'),
+          to: new Date('November 9, 2018 15:40:00 AM'),
+          type: 'work',
+        }
+      }},
 };
