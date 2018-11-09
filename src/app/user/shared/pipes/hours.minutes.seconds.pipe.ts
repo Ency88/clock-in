44b91c448bc759acf4dbfd57643d9182d0d7e154
Erik Jakuba <erikjakuba777@gmail.com>
2018-11-09 17:44:41 +0100

    pull request fix 1

---
+++ src/app/user/shared/pipes/hours.minutes.seconds.pipe.ts
@@ -0,0 +1,15 @@
+import { Injectable, Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'hoursMinutesSeconds',
+})
+@Injectable()
+export class HoursMinutesSeconds implements PipeTransform {
+  transform(value, args?) {
+    const minutes = Math.floor(value / 60);
+    const hours = Math.floor(minutes / 60);
+    const seconds = Math.floor(value % 60);
+
+    return hours + ' hrs, ' + minutes + ' mins, ' + seconds + ' secs';
+  }
+}
