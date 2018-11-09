0d8c039fb1c9575fdcd586b991d7dc36b6bb2e10
Erik Jakuba <erikjakuba777@gmail.com>
2018-11-09 17:48:08 +0100

    user dashboard

--- src/app/user/pages/user-dashboard/user-dashboard.component.ts
+++ src/app/user/pages/user-dashboard/user-dashboard.component.ts
@@ -1,33 +1,30 @@
-import { Component } from '@angular/core';
-import { map } from 'rxjs/operators';
-import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
+import {Component, OnInit} from '@angular/core';
+import {TimeLoggerService} from '../../../services/time.logger.service';
 
 @Component({
   selector: 'ci-user-dashboard',
   templateUrl: './user-dashboard.component.html',
   styleUrls: ['./user-dashboard.component.scss'],
 })
-export class UserDashboardComponent {
-  /** Based on the screen size, switch from standard to one column per row */
-  cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
-    map(({ matches }) => {
-      if (matches) {
-        return [
-          { title: 'Card 1', cols: 1, rows: 1 },
-          { title: 'Card 2', cols: 1, rows: 1 },
-          { title: 'Card 3', cols: 1, rows: 1 },
-          { title: 'Card 4', cols: 1, rows: 1 },
-        ];
-      }
+export class UserDashboardComponent implements OnInit {
+  public date: Date;
+  public formattedDate = '';
+  constructor(public timeLoggerService: TimeLoggerService) {}
 
-      return [
-        { title: 'Card 1', cols: 2, rows: 1 },
-        { title: 'Card 2', cols: 1, rows: 1 },
-        { title: 'Card 3', cols: 1, rows: 2 },
-        { title: 'Card 4', cols: 1, rows: 1 },
-      ];
-    })
-  );
+  ngOnInit() {
+    this.date = new Date();
+    this.formattedDate = this.date.getDate() + '.' + this.date.getMonth() + '.' + this.date.getFullYear();
+  }
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  processTracking() {
+    this.timeLoggerService.toggleTracking();
+    const subscription = this.timeLoggerService.intervalObservable
+      .subscribe(() => {
+        this.timeLoggerService.timer++;
+        this.timeLoggerService.calculateProgressValue();
+        if (this.timeLoggerService.tracking === false) {
+          subscription.unsubscribe();
+        }
+      });
+  }
 }
