91e5dd60930fdda349a1783ff0600758c69383a9
Erik Jakuba <erikjakuba777@gmail.com>
2018-11-09 17:47:24 +0100

    time logger

--- src/app/services/time.logger.service.ts
+++ src/app/services/time.logger.service.ts
@@ -1,33 +1,20 @@
 import { Injectable } from '@angular/core';
 import { IntervalObservable } from 'rxjs-compat/observable/IntervalObservable';
-import { MatProgressSpinner, MatSnackBar } from '@angular/material';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TimeLoggerService {
   public timer = 0;
-  private intervalObservable;
+  intervalObservable;
   public tracking: Boolean = false;
-  public progress_spinner: MatProgressSpinner;
   public progressValue = 0;
 
-  constructor(private snackBar: MatSnackBar) {}
-
-  public setProgressSpinner(progress_spinner: MatProgressSpinner) {
-    this.progress_spinner = progress_spinner;
-  }
+  constructor() {}
 
   startTracking() {
     this.tracking = true;
     this.intervalObservable = IntervalObservable.create(1000);
-    this.intervalObservable = this.intervalObservable.subscribe(() => {
-      if (this.tracking === false) {
-        this.intervalObservable.unsubscribe();
-      }
-      this.timer++;
-      this.calculateProgressValue();
-    });
   }
 
   calculateProgressValue() {
@@ -35,16 +22,22 @@ export class TimeLoggerService {
     const defaultLimit = 10; // for demo purposes 10 secs
     this.progressValue = (this.timer / defaultLimit) * 100;
     if (this.progressValue === 100) {
+      this.progressValue = 0;
+      this.timer = 0;
       this.stopTracking();
-      this.snackBar.open('Time limit reached. Go home', '', {
-        duration: 1500,
-        horizontalPosition: 'end',
-      });
       // send data to firebase
     }
   }
 
   stopTracking() {
     this.tracking = false;
+  }
+
+  toggleTracking() {
+    if (this.tracking === false) {
+      this.startTracking();
+    } else {
+      this.stopTracking();
+    }
   }
 }
