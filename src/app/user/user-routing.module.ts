cb38700117f659f9ee573e83e5730ef7c4ec9a98
Erik Jakuba <erikjakuba777@gmail.com>
2018-11-09 17:48:42 +0100

    user routing

--- src/app/user/user-routing.module.ts
+++ src/app/user/user-routing.module.ts
@@ -2,17 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { UserDashboardComponent } from './pages/user-dashboard/user-dashboard.component';
-import { TimeLoggerComponent } from './pages/time-logger/time-logger.component';
 
 const routes: Routes = [
   {
     path: 'dashboard',
     component: UserDashboardComponent,
-  },
-  {
-    path: 'time-logger',
-    component: TimeLoggerComponent,
-  },
+  }
 ];
 
 @NgModule({
