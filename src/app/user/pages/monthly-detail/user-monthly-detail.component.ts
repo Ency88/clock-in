cc647dab16922451dad4899d56bdce0c94f07d09
Ency88 <e.kudra@gmail.com>
2018-11-13 07:07:21 +0100

    Service for user times was introduced

--- src/app/user/pages/monthly-detail/user-monthly-detail.component.ts
+++ src/app/user/pages/monthly-detail/user-monthly-detail.component.ts
@@ -1,44 +1,29 @@
-import { Component } from '@angular/core';
-import { map } from 'rxjs/operators';
-import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
+import { Component, OnInit } from '@angular/core';
+import { switchMap } from 'rxjs/operators';
 import { Attendance } from '../../../models/attendance.model';
 import { TimeRecord } from '../../../models/time.record.model';
 import { TimeRecordEnum } from '../../../models/time.record.enum';
+import { Observable } from 'rxjs';
+import { UserTimesService } from '../../shared/user-times.service';
+import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'ci-user-monthly-detail',
   templateUrl: './user-monthly-detail.component.html',
   styleUrls: ['./user-monthly-detail.component.scss'],
 })
-export class UserMonthlyDetailComponent {
-  panelOpenState: boolean;
-  workType: TimeRecordEnum = TimeRecordEnum.work;
-  attendancesArray = [
-    {
-      day: 'MO',
-      date: new Date(2018, 10, 28),
-      timeRecords: [
-        { from: new Date(2018, 10, 28, 7, 15), to: new Date(2018, 10, 28, 10, 55), type: 'work' },
-        { from: new Date(2018, 10, 28, 11, 30), to: new Date(2018, 10, 28, 15, 10), type: 'work' },
-      ],
-    },
-    {
-      day: 'TU',
-      date: new Date(2018, 10, 28),
-      timeRecords: [
-        { from: new Date(2018, 10, 28, 7, 30), to: new Date(2018, 10, 28, 9, 30), type: 'work' },
-        { from: new Date(2018, 10, 28, 10, 30), to: new Date(2018, 10, 28, 11, 38), type: 'work' },
-      ],
-    },
-  ];
+export class UserMonthlyDetailComponent implements OnInit {
+  public panelOpenState: boolean;
+  public workType: TimeRecordEnum = TimeRecordEnum.work;
+  public attendances: Observable<any>;
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(private userTimeService: UserTimesService, private authService: AuthService) {}
 
-  attendances = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
-    map(() => {
-      return this.attendancesArray;
-    })
-  );
+  ngOnInit(): void {
+    this.attendances = this.authService.user.pipe(
+      switchMap(user => this.userTimeService.getAttendanceForUser(user.uid, new Date(), new Date()))
+    );
+  }
 
   headerText(attendance: Attendance) {
     return (
@@ -58,7 +43,6 @@ export class UserMonthlyDetailComponent {
     result = result / 1000;
     const hours = Math.floor(result / 3600);
     const minutes = Math.floor((result % 3600) / 60);
-    const seconds = result % 60;
 
     return hours + ' hrs, ' + minutes + ' mins';
   }
